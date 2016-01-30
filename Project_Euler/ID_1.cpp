b4c39f23fd31d560d16bd34d560f8646bf0a4bf7
lukeroweprivate@gmail.com <lukeroweprivate@gmail.com>
2016-01-29 19:53:15 -0800

    Added .gitignore /Debug/ and did the first problem, ID_1.cpp

---
+++ Project_Euler/ID_1.cpp
@@ -0,0 +1,31 @@
+/*
+* ID_1.cpp
+*
+*  Created on: Jan 29, 2016
+*      Author: Luke
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int in = 1;
+    int sum;
+
+    for (in = 1; in < 1000; in++) {
+
+        if (in % 3 == 0 || in % 5 == 0) {
+            cout << in << endl;
+
+            sum = sum + in;
+
+        }
+
+    }
+
+    cout << sum;
+
+    return 0;
+}
