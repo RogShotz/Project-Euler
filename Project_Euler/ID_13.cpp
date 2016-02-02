d4bdf259b39a58ffcf4015e556d5da895a07ce1c
lukeroweprivate@gmail.com <lukeroweprivate@gmail.com>
2016-02-01 23:21:52 -0800

    Going to have to add a function to this to add 50 digits to 50 digits, of course variable from user input, i should also make it so that the code can auto detect how many cin's a users input will have.

---
+++ Project_Euler/ID_13.cpp
@@ -0,0 +1,38 @@
+/*
+ * ID_13.cpp
+ *
+ *  Created on: Feb 1, 2016
+ *      Author: Luke
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	string largeNumb;
+	int digit;
+	int outdig;
+	long sum;
+	int cintotal;
+	int goTill;
+
+	cout << "Please tell me how many line of numbers are you going to input?" << endl;
+	cin >> goTill;
+	cout << "If you answered anything more that one, just paste all of it in." << endl << "Have fun putting in your large number";
+	goTill = goTill++;
+	for (cintotal = 0; cintotal < goTill; cintotal++){
+	cin >> largeNumb;
+
+	for (digit = 0; digit < largeNumb.size(); digit++) {
+		largeNumb.at(digit) >= '0' && largeNumb.at(digit) <= '57';
+		outdig = largeNumb.at(digit) - 48;
+		sum = sum + largeNumb.at(digit);
+		cout << outdig << " " << sum << endl;
+
+	}
+	}
+
+	return 0;
+}
