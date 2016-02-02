8731029b46bc698f4f88fc2712fc1da36292a846
lukeroweprivate@gmail.com <lukeroweprivate@gmail.com>
2016-02-01 23:20:44 -0800

    Mainly broke stuff. To be cont. later, im posting so i can compare changes incase i forget a thought.

--- Project_Euler/ID_3.cpp
+++ Project_Euler/ID_3.cpp
@@ -5,6 +5,39 @@
  *      Author: Luke
  */
 
+#include <iostream>
+
+using namespace std;
+//I know i'm not going to use anything but standard
+
+int MymainMAN() {
+
+	long count;
+	long sample;
+	long prevPrime;
+
+	cin >> sample;
+
+	for (count = 1; sample >= count + 1; count++) {
+		if (sample % count == 0) {
+			if (sample % prevPrime){
+
+				cout << count << " Dividable" << endl;
+			}
+			else{
+
+			}
 
 
+	}
+	else {
+
+		cout << count << " Not Dividable" << endl;
+
+	}
+}
+
+
+return 0;
+}
 
