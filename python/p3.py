14a13bb3ee9c8ba70102a1dd5dadba532a97e8b3
Luke Rowe <RogShotz@users.noreply.github.com>
2021-08-05 22:18:29 -0700

    Better method

--- python/p3.py
+++ python/p3.py
@@ -1,17 +1,9 @@
-primes = [2]
-prime = True
-number = 600851475143
+cap = 600851475143
+primes = []
 
-if number % 2 != 0:  # function makes it even so we can keep is as a int
-    number += 1
-    number /= 2  # we know anything past half of it doesn't matter
+for y in range(2, cap + 1):  # construct check, only needs primes
+    print(y)
+    if all(y % n != 0 for n in primes):
+        primes.append(y)
 
-for i in range(3, int(number), 2):  # increment by 2 to make it 2x faster
-    for j in range(len(primes)):
-        if i % primes[j] == 0:
-            prime = False
-
-    if prime:
-        print(i)
-        primes.append(i)
-    prime = True
+print(primes[len(primes-1)])
