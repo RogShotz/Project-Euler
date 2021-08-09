33ddc95e8a0447619303b0a1baa05e581251b598
Luke Rowe <RogShotz@users.noreply.github.com>
2021-08-08 20:51:45 -0700

    Create p8.py

---
+++ python/p8.py
@@ -0,0 +1,26 @@
+def popstring():  # populate string
+    f = open("p8/unparsednumber.txt", "r")
+
+    lines = f.readlines()
+    for line in lines:
+        global str
+        line = line.replace("\n", "")
+        str += line
+    return str
+
+
+popstring()
+
+intlist = list(map(int, str))
+
+greatest = 0
+
+for x in range(0, len(intlist) - 13):  # -13 to pad end of list
+    sum = intlist[x]
+    for i in range(x, x + 12):  # start at x, +12 till the end
+        print(sum)
+        sum *= intlist[i]
+
+    if greatest < sum:
+        greatest = sum
+print(greatest)
